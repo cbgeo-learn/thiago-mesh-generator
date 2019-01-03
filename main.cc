8c647ee7f77f21d34fcff5c9b37a782daa5daa7b
Thiago Ordonho Araujo <thiago.araujo@utexas.edu>
2019-01-03 15:58:46 -0600

    :art: last adjustments

--- main.cc
+++ main.cc
@@ -29,7 +29,7 @@ int main() {
 
   // Initialize spacing and number of points in each direction
   double vertical_spacing, horizontal_spacing;
-  unsigned int nvertical_points, nhorizontal_points;
+  unsigned nvertical_points, nhorizontal_points;
 
   // Check mesh type choice
   if (choice == 1) {
@@ -68,22 +68,23 @@ int main() {
   std::cout << "total number of horizontal points: " << nhorizontal_points
             << std::endl;
 
-  // Declare an auxiliary point
-  std::array<double, 2> aux_point;
-
   // Loop over all the vertical ('i' represents each column) and horizontal ('j'
   // represents each row) points and add each of them to the list of points by
   // assigning their position.
-  for (int i = 0; i < nvertical_points; ++i)
+  for (int i = 0; i < nvertical_points; ++i) {
     for (int j = 0; j < nhorizontal_points; ++j) {
+      // Declare an auxiliary point
+      std::array<double, 2> aux_point;
+
       aux_point.at(0) = j * horizontal_spacing;
       aux_point.at(1) = i * vertical_spacing;
       points.emplace_back(aux_point);
     }
+  }
 
   // Print the (x,y) values of each point
   int i = 1;  // helps counting the points to be printed
-  for (auto& point : points) {
+  for (const auto& point : points) {
     std::cout << "Point #" << i << " (" << point.at(0) << "," << point.at(1)
               << ")\n";
     ++i;
